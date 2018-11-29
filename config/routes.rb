65f6a831a8f7c3eaa27e50545687ecce4fb2d5c5
Rhayser <rhayser@outlook.com>
2018-11-29 00:21:41 -0300

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-config.assets.initialize_on_precompile = false
 Rails.application.routes.draw do
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
